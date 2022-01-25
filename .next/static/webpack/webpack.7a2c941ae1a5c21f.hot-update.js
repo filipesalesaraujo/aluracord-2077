32fe46e88ee26659cb157e3aa1c99aa913f1c722
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-01-25 06:16:37 -0300

    styles, @skynexui/components

--- .next/static/webpack/webpack.7a2c941ae1a5c21f.hot-update.js
+++ .next/static/webpack/webpack.7a2c941ae1a5c21f.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "43f31a80dafca500"; }
+/******/ 	__webpack_require__.h = function() { return "7ee18e8475ea8727"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/harmony module decorator */
